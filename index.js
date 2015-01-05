f1d3c32edb1753a26b5f41e38e61a04c08854770
Peter J. Holfelder <peter.j.holfelder@gmail.com>
2015-01-04 20:10:01 -0500

    Added Red Hat Openshift environment variables for startup.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express')
   , http = require('http');
  
- var app = express();
- var server = http.createServer(app);
+var app = express();
+var server = http.createServer(app);
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.get('/', function(req, res) {
   res.send('this is where peter holfelder\'s node resume will be built.');
 });
 
-app.listen(3000)
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
