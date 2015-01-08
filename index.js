df3ca258958130b5d6335e935de3c213b12d7988
Peter J. Holfelder <peter.j.holfelder@gmail.com>
2015-01-07 19:52:29 -0500

    Added static directory /public.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 // Initialize templating.
 app.set('views', './views');
 app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.render('index');
